c48c790c5a482f874729951e17aa91fdc0f6950a
Dharani I <dharaniilango1209@gmail.com>
2024-07-02 23:05:49 +0530

    Language Basics Module Files

---
+++ Java_Fundamentals/Language_Basics/Cla3.java
@@ -0,0 +1,7 @@
+public class Cla3{
+    public static void main(String[] args){
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        System.out.println("The sum of "+args[0]+" and "+args[1]+" is."+(num1+num2));
+    }
+}
